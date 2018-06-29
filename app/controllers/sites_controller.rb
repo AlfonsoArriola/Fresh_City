f5168950ee217cd527f29bf32cc75d88da9e55db
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-06-29 10:27:21 -0400

    added routes and setup for sites controller.  added index view for sites

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,2 +1,32 @@
 class SitesController < ApplicationController
+	def index
+		@sites = Site.all
+	end
+	def create
+		@site = Site.create(site_params)
+	end
+	def new
+		@site = Site.new
+	end
+	def edit
+		@student = Site.find(params[:id])
+	end
+	def show
+		@site = Site.find(params[:id])
+	end
+	def update
+	    site = Site.find(params[:id])
+	    site.update(site_params)
+	    redirect_to sites_path	
+	end
+	def destroy
+		Site.destroy(params[:id])
+		redirect_to sites_path
+	end
+
+	private
+
+	def site_params
+		params.require(:site).permit()
+	end
 end
