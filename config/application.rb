f7b0088a497c51d4cbe82a3350fd442a73518ae5
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-07-03 13:13:12 -0400

    added chinese, arabic, and russian translations

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,8 @@ module FreshCity
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.available_locales = [:en, :ru, :es, :zh-CN, :ar]
+    config.assets.enabled = true
+    config.assets.paths << Rails.root.join('/app/assets/fonts')
   end
 end
