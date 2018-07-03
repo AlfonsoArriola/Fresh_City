434506428b64c520c8ded9ad29d5e2a86f9eba6b
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-07-02 21:15:55 -0400

    added lat and long to sites model, added geocoder gem, now im doing weird stuff with secrets.yml

---
+++ db/migrate/20180703003216_add_latitude_to_sites.rb
@@ -0,0 +1,5 @@
+class AddLatitudeToSites < ActiveRecord::Migration[5.2]
+  def change
+    add_column :sites, :latitude, :float
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_29_212430) do
+ActiveRecord::Schema.define(version: 2018_07_03_003354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 2018_06_29_212430) do
     t.string "note"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
 end
