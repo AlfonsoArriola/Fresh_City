e439a04814f84a7dc05738ced580c531074cf7ee
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-07-04 08:11:34 -0400

    fixing nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,21 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
      root 'home#index'
-  
-     resources :sites
+
      resources :prospect_sites
+
+  
+     resources :sites do
+     	resources :trains
+     end
+
+     resources :trains do
+     	resources  :sites
+     end
+
+     resources :site_trains do
+     	resources :trains
+     	resources :sites
+     end
 
 end
