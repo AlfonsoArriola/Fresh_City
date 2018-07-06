f840fa528edc0614a5bcc36baf965a0767cc0a63
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-07-06 11:05:36 -0400

    do not merge

--- app/models/site.rb
+++ app/models/site.rb
@@ -5,5 +5,6 @@ class Site < ApplicationRecord
 	validates :name, :address, :gender, :days_of_the_week_and_hours_of_operation , :towel, :shower_shoe, :soap, :feminine_hygiene,   :presence => true, :uniqueness => true
 	has_many :site_trains
 	has_many :trains, through: :site_trains
+	has_one :borough
 
 end
