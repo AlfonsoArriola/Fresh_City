10a48e6bce6c0442ca52d56832509b6c51b4099b
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-07-15 10:17:35 -0400

    updates

--- app/assets/javascripts/the_bronx.js
+++ app/assets/javascripts/the_bronx.js
@@ -1,2 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+// function initialize() {
+//         let site_location = {lat: @site.latitude, lng: @site.longit}
+//         let map = new google.maps.Map(document.getElementById('map'), {
+//           center: site_location,
+//           zoom: 14
+//         });
+//         var panorama = new google.maps.StreetViewPanorama(
+//             document.getElementById('pano'), {
+//               position: site_location,
+//               pov: {
+//                 heading: 34,
+//                 pitch: 10
+//               }
+//             });
+//         map.setStreetView(panorama);
+// }
