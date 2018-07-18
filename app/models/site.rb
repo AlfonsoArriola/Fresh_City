905d93959931648ec3d33f1aac41531ab7a8492e
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-07-18 06:42:08 -0400

    now can upload one photo per site

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,10 +1,11 @@
 class Site < ApplicationRecord
 	geocoded_by :address
 	after_validation :geocode
-
+	has_one_attached :image
 	# validates :name, :cost :address, :phone, :location_type, :gender, :age_range, :days_of_the_week_and_hours_of_operation, :towel, :shower_shoe, :soap, :feminine_hygiene, :note, :website, :borough_id,   :presence => true, :uniqueness => true
 	has_many :trains_sites
 	has_many :trains, through: :trains_sites
 	belongs_to :borough
+
 
 end
