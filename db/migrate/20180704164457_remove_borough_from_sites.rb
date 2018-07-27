8bda40822076bc2fa91bd80cf670e3b83e2d3a90
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-07-27 12:24:55 -0400

    routing fix

--- db/migrate/20180704164457_remove_borough_from_sites.rb
+++ db/migrate/20180704164457_remove_borough_from_sites.rb
@@ -1,5 +1,6 @@
 class RemoveBoroughFromSites < ActiveRecord::Migration[5.2]
   def change
+  	# below was commented out due to it messing up the heroku db:migration
     # remove_column :sites, :borough, :string
   end
 end
