a7c2623d1a933ab908d73c7ef34abd5e627af47c
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-07-30 04:56:26 -0400

    si and bk id fix

--- app/controllers/staten_island_controller.rb
+++ app/controllers/staten_island_controller.rb
@@ -1,13 +1,13 @@
 class StatenIslandController < ApplicationController
 	def index
 		sites = Site.all
-		si_sites = sites.where(borough_id: 4)
+		si_sites = sites.where(borough_id: 6)
 		@si_free_sites = si_sites.where("cost like ?", "%Free%")
 		@si_cheap_sites = si_sites.where("cost like ?", "See Notes%")	
 	end
 	def show
      	sites = Site.all
-		si_sites = sites.where(borough_id: 4)
+		si_sites = sites.where(borough_id: 6)
 		@site = si_sites.find(params[:id])		
      end
 end
