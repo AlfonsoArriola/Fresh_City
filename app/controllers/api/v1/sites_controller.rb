53d292a9e2ae98be8b4921c20fb3a887c7d0f568
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-11-06 07:18:08 -0500

    Rest Api has been added

---
+++ app/controllers/api/v1/sites_controller.rb
@@ -0,0 +1,28 @@
+module Api
+ module V1
+  class SitesController < ApplicationController
+   	 respond_to :json
+
+   	 def index
+   	 	respond_with Site.all
+   	 end
+
+   	 def show
+   	 	respond_with Site.find(params[:id])
+   	 end
+
+   	 # def create
+   	 # 	respond_with Site.create(params[:site])
+   	 # end
+
+   	 # def update
+   	 # 	respond_with Site.update(params[:id], params[:site])
+   	 # end
+
+   	 # def destroy
+   	 # 	respond_with Site.destroy(params[:id])
+   	 # end
+
+   end
+ end
+end
